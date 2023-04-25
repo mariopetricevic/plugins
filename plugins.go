c69d80ab11f84d3d8dc501cc5d9a2de4424e23de
mariopetricevic <68001932+mariopetricevic@users.noreply.github.com>
2023-04-25 14:33:35 +0200

    Add files via upload

---
+++ plugins.go
@@ -0,0 +1,31 @@
+package plugins
+
+import (
+	"context"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/kubernetes/pkg/scheduler/framework"
+)
+type customFilterPlugin struct{
+	handle framework.Handle
+}
+
+
+//var _ framework.FilterPlugin = &customFilterPlugin{}
+var _  = framework.FilterPlugin(&customFilterPlugin{})
+
+func (p *customFilterPlugin) Name() string {
+	return "CustomFilter"
+}
+
+
+
+func (p *customFilterPlugin) Filter(ctx context.Context, state *framework.CycleState, pod *v1.Pod, nodeInfo *framework.NodeInfo) *framework.Status {
+	// Implementirajte logiku filtriranja čvorova ovdje
+	return framework.NewStatus(framework.Success)
+}
+
+func New(obj runtime.Object, handle framework.FrameworkHandle) (framework.Plugin, error) {
+	return &customFilterPlugin{}, nil
+}
+
