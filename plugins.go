a702991a40f5356501d1af81685dbe4623b47010
mariopetricevic <68001932+mariopetricevic@users.noreply.github.com>
2023-05-30 15:13:10 +0200

    Update plugins.go

--- plugins.go
+++ plugins.go
@@ -4,362 +4,164 @@ import (
 	"context"
 	"fmt"
 	"sort"
+	"strconv"
 	"strings"
 
-	"strconv"
-
-//	"github.com/go-ping/ping"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/kubernetes/pkg/scheduler/framework"
 )
 
-type customFilterPlugin struct {
+type MyK3SPlugin struct {
 	handle framework.Handle
+}
+
+type NodeNameDistance struct {
+	Name     string
+	Distance int
 }
 
 const (
 	// Name : name of plugin used in the plugin registry and configurations.
-	Name = "CustomFilterPlugin"
+	Name = "MyK3SPlugin"
 )
 
-// var _ framework.FilterPlugin = &customFilterPlugin{}
-var _ = framework.FilterPlugin(&customFilterPlugin{})
+var _ = framework.FilterPlugin(&MyK3SPlugin{})
+var _ = framework.ScorePlugin(&MyK3SPlugin{})
 
-var _ = framework.ScorePlugin(&customFilterPlugin{})
-
-func (p *customFilterPlugin) Name() string {
+func (p *MyK3SPlugin) Name() string {
 	return Name
 }
 
-func (p *customFilterPlugin) Filter(ctx context.Context, state *framework.CycleState, pod *v1.Pod, nodeInfo *framework.NodeInfo) *framework.Status {
+func (p *MyK3SPlugin) Filter(ctx context.Context, state *framework.CycleState, pod *v1.Pod, nodeInfo *framework.NodeInfo) *framework.Status {
+	//
+	// Filter nodes that can run the pod. If node cannot run the pod
+	// method returns framework status Unschedulable.
+	// Additionaly, method filters nodes that already have pod running
+	// with the same application name.
+	//
+	fmt.Println("filter pod:", pod.Name, ", application name: ", pod.Labels["applicationName"], ", Node: ", nodeInfo.Node().Name)
+	fmt.Println("Node resources: CPU:", nodeInfo.Node().Status.Allocatable.Cpu(), ", memory: ", nodeInfo.Node().Status.Allocatable.Memory())
 
-	//fmt.Println("------------------FILTER-------------------------------------------")
-
-	//fmt.Println("-----------------INFORMACIJE O ČVORU-----------------")
-	//resursi cvora
+	//availableResources := nodeInfo.Node().Status.Allocatable
 	nodeCpu := nodeInfo.Node().Status.Capacity[v1.ResourceCPU]
-	//fmt.Println("---node cpu, cpu, name, adresa: ")
-	//fmt.Println("----", nodeCpu.String(), nodeCpu, nodeInfo.Node().Name, nodeInfo.Node().Status.Addresses[0].Address)
-	//fmt.Println("-----------------INFORMACIJE O ČVORU END---------------")
-	//fmt.Println()
-	//fmt.Println()
 
-	//fmt.Println("-----------------INFORMACIJE O PODU-----------------")
-	//podLabelValue := pod.Labels["scheduleon"]
+	//calculate allocated CPU for running all containters in current pod
 	var podCPU resource.Quantity
-	//fmt.Println("---LABELA PODA:", podLabelValue)
-	//fmt.Println("-----------------INFORMACIJE O PODU END-----------------")
-	//fmt.Println()
-	//fmt.Println()
-
-	//fmt.Println("-----------------IZVRŠAVANJE KODA START-----------------")
-
 	for _, container := range pod.Spec.Containers {
-		//fmt.Println("------u for petlji za resurse: ")
 		if cpu, ok := container.Resources.Requests[v1.ResourceCPU]; ok {
 			podCPU.Add(cpu)
-		//	fmt.Println("------Printam cpu poda:", cpu)
-		} else {
-		//	fmt.Println("------nista ")
 		}
 	}
 
-	//smh := false
-	//ako su resursi zadovoljavajuci moze dalje
-	if nodeCpu.Cmp(podCPU) > 0 { //smh dio koda treba maknuti jer trenutno ne radi ovo s provjerom resursa
-		//fmt.Println("---DOVOLJNO RESURSA", nodeCpu.Cmp(podCPU))
-		
-		// provjeri ima li na cvoru pod na kojem se vec vrti aplikacija koja se zeli postavit
+	//If requred resources for running pod are less than available resources on a node
+	if nodeCpu.Cmp(podCPU) > 0 {
+
+		// Check if node already runs application with the same name
 		pods := nodeInfo.Pods
 		for _, p := range pods {
 			labels := p.Pod.GetLabels()
+			//if any pod running on current node already has pod with the label applicationName equal to pod given as parameter, then this app
+			// already exists on this node.
+			if applicationName, found := labels["applicationName"]; found && applicationName == pod.Labels["applicationName"] {
 
-			//ako je na nekom od podova koji se vrte na trenutnom čvoru applicationName jednak imenu aplikacije poda koji smo predali kao parametar, preskacemo taj čvor
-			//ako već postoji aplikacija koju se želi schedulat na trenutnom čvoru preskoči
-			if applicationName, found := labels["app"]; found && applicationName == pod.Labels["app"] {
-				fmt.Println("Ova aplikacija %s vec postoji na cvoru %s", pod.Labels["app"], nodeInfo.Node().Name)
-				//continue
-				return framework.NewStatus(framework.Unschedulable, "aplikacija postoji na ovom cvoru")
+				fmt.Println("Application:", pod.Labels["applicationName"], "Already exists on node: ", nodeInfo.Node().Name)
+
+				return framework.NewStatus(framework.Unschedulable, "Application already exists on this node")
 			}
 		}
-		//fmt.Println("------------------FILTER END-------------------------------------------")
 		return framework.NewStatus(framework.Success)
 	} else {
-		//fmt.Println("---NEDOVOLJNO RESURSA")
-		//fmt.Println("------------------FILTER END-------------------------------------------")
-		return framework.NewStatus(framework.Unschedulable, "NEDOVOLJNO RESURSA")
+		return framework.NewStatus(framework.Unschedulable, "Not enough resources to run application: ", pod.Labels["applicationName"])
 	}
-
-	// if podLabelValue == "agent2node" && nodeInfo.Node().Name == "agent2node" {
-
-	// 	fmt.Println("---agent2node je true")
-	// 	//ako se radi o cvoru na koji trebamo schedulat, provjeri njegove resurse
-	// 	// for _, container := range pod.Spec.Containers {
-	// 	// 	fmt.Println("------u for petlji za resurse: ")
-	// 	// 	if cpu, ok := container.Resources.Requests[v1.ResourceCPU]; ok {
-	// 	// 		podCPU.Add(cpu)
-	// 	// 		fmt.Println("------Printam cpu poda:")
-	// 	// 		fmt.Println(cpu)
-	// 	// 	} else {
-	// 	// 		fmt.Println("------nista ")
-	// 	// 	}
-	// 	// }
-
-	// 	smh := false
-	// 	//ako su resursi zadovoljavajuci stavi pod na taj node
-	// 	if nodeCpu.Cmp(podCPU) > 0 && smh { //smh dio koda treba maknuti jer trenutno ne radi ovo s provjerom resursa
-	// 		fmt.Println("---ovdje da vratim success")
-	// 		fmt.Println(nodeCpu.Cmp(podCPU))
-	// 		return framework.NewStatus(framework.Success)
-	// 	} else {
-	// 		//ako nisu, onda logika za ping i trazi najblizi cvor drugi
-	// 		fmt.Println("---nedovoljno resursa, trazim drugi node ....")
-
-	// 		if p.handle == nil {
-	// 			fmt.Println("---handle je nulll....")
-	// 		}
-
-	// 		nodeLister := p.handle.SnapshotSharedLister().NodeInfos()
-	// 		if nodeLister == nil {
-	// 			fmt.Println("---nodes lister je nullll....")
-	// 		}
-
-	// 		nodes, err := p.handle.SnapshotSharedLister().NodeInfos().List()
-	// 		if err != nil {
-	// 			fmt.Println("---ovo tu je null sta li...")
-	// 			return framework.NewStatus(framework.Error, "Error getting node list")
-	// 		}
-	// 		fmt.Println("---pronaso informacije o nodovima. Ispis nodova:")
-
-	// 		for _, node := range nodes {
-	// 			fmt.Println(node.Node().Name)
-	// 			fmt.Println(node.Node().Status.Addresses[0].Address)
-	// 		}
-	// 		fmt.Println("gotov ispis nodova")
-
-	// 		// var closestNode *framework.NodeInfo
-	// 		// minRTT := time.Duration(math.MaxInt64)
-	// 		// for _, node := range nodes {
-	// 		// 	rtt, err := pingNode(node.Node().Status.Addresses[0].Address)
-	// 		// 	fmt.Println("ispis rtta")
-	// 		// 	fmt.Println(node.Node().Name)
-	// 		// 	fmt.Println(rtt)
-	// 		// 	if err != nil {
-	// 		// 		fmt.Println("error dohvcanja rtta")
-	// 		// 		continue
-	// 		// 	}
-	// 		// 	if rtt < minRTT {
-	// 		// 		fmt.Println("ispis rtta")
-	// 		// 		fmt.Println(rtt)
-	// 		// 		minRTT = rtt
-	// 		// 		closestNode = node
-	// 		// 	}
-	// 		// 	fmt.Println("proso po svima")
-	// 		// }
-
-	// 		// fmt.Println("-----INFORMACIJE O NAJBLIZEM ČVORU-----------------")
-	// 		// //resursi cvora
-	// 		// nodeCpu := closestNode.Node().Status.Capacity[v1.ResourceCPU]
-	// 		// fmt.Println("node cpu je: ")
-	// 		// fmt.Println(nodeCpu.String())
-	// 		// fmt.Println(nodeCpu)
-	// 		// fmt.Println("NODE NAME JE: ")
-	// 		// fmt.Println(closestNode.Node().Name)
-	// 		// fmt.Println("ADRESA JE: ")
-	// 		// fmt.Println(closestNode.Node().Status.Addresses[0].Address)
-	// 		// fmt.Println("-----INFORMACIJE O NAJBLIZEM ČVORU END---------------")
-
-	// 		//&& closestNode.Node().Name == nodeInfo.Node().Name
-	// 		// Ako je najbliži čvor trenutni čvor, vrati Success
-	// 		// if closestNode != nil {
-	// 		// 	fmt.Println("---pronaden najblizi cvor pomocu pinga")
-	// 		// 	fmt.Println("-----INFORMACIJE O NAJBLIZEM ČVORU-----------------")
-	// 		// 	//resursi cvora
-	// 		// 	nodeCpu := closestNode.Node().Status.Capacity[v1.ResourceCPU]
-	// 		// 	fmt.Println("node cpu je: ")
-	// 		// 	fmt.Println(nodeCpu.String())
-	// 		// 	fmt.Println(nodeCpu)
-	// 		// 	fmt.Println("NODE NAME JE: ")
-	// 		// 	fmt.Println(closestNode.Node().Name)
-	// 		// 	fmt.Println("ADRESA JE: ")
-	// 		// 	fmt.Println(closestNode.Node().Status.Addresses[0].Address)
-	// 		// 	fmt.Println("-----INFORMACIJE O NAJBLIZEM ČVORU END---------------")
-	// 		// 	return framework.NewStatus(framework.Success)
-	// 		// } else {
-	// 		// 	fmt.Println("nemoguce schedulatt")
-	// 		// 	return framework.NewStatus(framework.Unschedulable, "nije moguce schedulat, trazi drugi cvor")
-	// 		// }
-
-	// 		//return framework.NewStatus(framework.Unschedulable, "nije moguce schedulat, trazi drugi cvor")
-	// 	}
-	// }
-
-	// fmt.Println("-----------------IZVRŠAVANJE KODA END-----------------")
-	// fmt.Println()
-	// fmt.Println()
-	// fmt.Println("------------------FILTER  END-------------------------------------------")
-	// return framework.NewStatus(framework.Unschedulable, "Node is not schedulable")
 }
 
-func (p *customFilterPlugin) Score(ctx context.Context, state *framework.CycleState, pod *v1.Pod, nodeName string) (int64, *framework.Status) {
+func (p *MyK3SPlugin) Score(ctx context.Context, state *framework.CycleState, pod *v1.Pod, nodeName string) (int64, *framework.Status) {
 
 	fmt.Println("--------------------SCORE-------------------------------------")
 	fmt.Println()
 
-	if p.handle == nil {
-		fmt.Println("---handle je nulll....")
-	}
-	
-	requestFromNode := pod.Labels["scheduleon"]
+	//deployments need to have label requestFrom which indicates on which node request for pod has came
+	requestFromNode := pod.Labels["requestFrom"] //scheduleon
 
 	nodes, err := p.handle.SnapshotSharedLister().NodeInfos().List()
 	if err != nil {
-		fmt.Println("---ovo tu je null sta li...")
+		fmt.Println("Error occured while getting all nodes in cluster.")
 	}
-	fmt.Println("---------Čvorovi u clusteru:-------------")
 
 	for _, node := range nodes {
-		fmt.Println("Ime: %s Adresa: %s", node.Node().Name, node.Node().Status.Addresses[0].Address)
-		//fmt.Println("---", node.Node().Status.Addresses[0].Address)
-
-		// if node.Node().Name == nodeName {
-		// 	//cvor s trenutnim imenom je prosao filtriranje, znaci ima dovoljno resursa, znaci mogli bi ga spremit u varijablu
-		// 	// currentNode
-		// }
+		fmt.Println("NodeName :", node.Node().Name, "Address: ", node.Node().Status.Addresses[0].Address)
 	}
-	fmt.Println("-------end čvorovi u clusteru-------------")
-	
-	fmt.Println("----------trenutni čvor je: '%s'-------------", nodeName)
-	
 
-	// podLabelValue := pod.Labels["scheduleon"]
+	fmt.Println("Calculating score for current node:", nodeName)
 
-	//dohvati cvor od kojega je stigao zahtjev
-	currentNodeInfo, err := p.handle.SnapshotSharedLister().NodeInfos().Get(requestFromNode)
-	currentNode := currentNodeInfo.Node()
-	currentNodeLabels := currentNode.GetLabels()
+	//Get node from which request came
+	requesterNodeInfo, err := p.handle.SnapshotSharedLister().NodeInfos().Get(requestFromNode)
+	requesterNode := requesterNodeInfo.Node()
+	requesterNodeLabels := requesterNode.GetLabels()
 
-	pingLabels := make(map[string]int)
+	//map of node names and distances
+	mapNodeNameDistance := make(map[string]int)
 
-	// prodi po svim labelama cvora i dohvati labele za udaljenosti izmedu cvorova
-	for label, value := range currentNodeLabels {
+	// get labels with node names in cluster and their distances
+	for label, value := range requesterNodeLabels {
 		if strings.HasPrefix(label, "ping-") {
-			intValue, err := strconv.Atoi(value)
+
+			distanceToNode, err := strconv.Atoi(value)
 			if err != nil {
-				fmt.Println("greska")
+				fmt.Println("Error while converting.")
 			}
 
-			pureLabel := strings.TrimPrefix(label, "ping-")  //CutPrefix(label, "ping-")
-			pingLabels[pureLabel] = intValue
-		}else{
+			nodeName := strings.TrimPrefix(label, "ping-")
+			mapNodeNameDistance[nodeName] = distanceToNode
 		}
 	}
-	fmt.Println("printam labele i value pinga.")
-	for key, value := range pingLabels{
-		fmt.Println(key, " val: ", value)
+
+	// Store node distances
+	var sortedNodeDistances []NodeNameDistance
+	for label, value := range mapNodeNameDistance {
+		sortedNodeDistances = append(sortedNodeDistances, NodeNameDistance{label, value})
 	}
 
-
-	//spremi u strukturu sve labele i njihove udaljenosti
-	var sortedPingValues []LabelPing
-	for label, value := range pingLabels {
-		sortedPingValues = append(sortedPingValues, LabelPing{label, value})
-	}
-
-	//sortiraj od najmanjeg do najveceg
-	sort.Slice(sortedPingValues, func(i, j int) bool {
-		return sortedPingValues[i].Value < sortedPingValues[j].Value
+	//sort node distances from lowest to highest
+	sort.Slice(sortedNodeDistances, func(i, j int) bool {
+		return sortedNodeDistances[i].Distance < sortedNodeDistances[j].Distance
 	})
 
-	fmt.Println("-------ispis sortiranih labela-------------")
-	for _, pingValuesForLabel := range sortedPingValues {
-		fmt.Printf("Label: %s, Value: %d\n", pingValuesForLabel.Label, pingValuesForLabel.Value)
+	fmt.Println("----------Sorted node distances-------------")
+	for _, nodeNameDistance := range sortedNodeDistances {
+		fmt.Printf("Label: %s, Value: %d\n", nodeNameDistance.Name, nodeNameDistance.Distance)
 	}
-	fmt.Println("-------end ispisa sortiranih labela-------------")
+	fmt.Println("-------End sorted node distances-------------")
 
-
+	//Scoring nodes
 	var score int = 0
-
-	if requestFromNode == nodeName{
+	if requestFromNode == nodeName {
 		score = 100
+		fmt.Println("Requested node: ", requestFromNode, " can run the pod. Scoring node with maximum score.")
 		return 100, nil
-	}else{
-		for _, pingValuesForLabel := range sortedPingValues {
-			//fmt.Printf("Label: %s, Value: %d\n", pingValuesForLabel.Label, pingValuesForLabel.Value)
-	
-			if pingValuesForLabel.Label == nodeName{
-				
-			score = 90 - pingValuesForLabel.Value
-				fmt.Println("scoring node: %s, score: %d", pingValuesForLabel.Label, score)
-			break
+	} else {
+		for _, nodeNameDistance := range sortedNodeDistances {
+			//find first (with lowest distance) node that can run a pod
+			if nodeNameDistance.Name == nodeName {
+
+				score = 90 - nodeNameDistance.Distance
+				fmt.Println("Scoring node: ", nodeNameDistance.Name, " score: ", score)
+				break
 			}
 		}
 	}
-	
-	
-
-	
-	
-
-
-	// pods, err := currentNode.Pods
-
-	// for _, pod := range pods {
-	// 	labels := pod
-
-	// }
-
-	// fmt.Println()
-	// fmt.Println()
-
-	// if nodeName == podLabelValue {
-	// 	fmt.Println("---ispis scorea za node")
-	// 	fmt.Println("---", nodeName)
-	// 	fmt.Println("--------------------SCORE END-------------------------------------")
-	// 	return 100, nil
-	// }
-
-	// nodeInfo, err := p.handle.SnapshotSharedLister().NodeInfos().Get(nodeName)
-	// if err != nil {
-	// 	fmt.Println("error")
-	// 	fmt.Println("--------------------SCORE END-------------------------------------")
-	// 	return 0, framework.NewStatus(framework.Error, fmt.Sprintf("Error getting node %s from Snapshot: %v", nodeName, err))
-	// }
-
-	// // rtt, err := pingNode(nodeInfo.Node().Status.Addresses[0].Address)
-	// // fmt.Println("---ISPIS RTTA:")
-	// // fmt.Println("----", nodeInfo.Node().Name)
-	// // fmt.Println("----", rtt)
-	// // fmt.Println("---ISPIS RTTA END")
-	// // if err != nil {
-	// // 	fmt.Println("error")
-	// // 	fmt.Println("--------------------SCORE END-------------------------------------")
-	// // 	return 0, framework.NewStatus(framework.Error, fmt.Sprintf("Error pinging node %s: %v", nodeName, err))
-	// }
-
-	// // Ovdje bi trebalo pretvoriti vrijeme odziva u ocjenu. Niže vrijeme odziva bi trebalo rezultirati višom ocjenom.
-	// //100 je max score koji se moze dobit, stavili smo 95 pa od njega oduzimamo
-	// score := 95 - int64(rtt.Milliseconds())
-	// fmt.Println("---ispis scorea za node:")
-	// fmt.Println("----", nodeInfo.Node().Name)
-	// fmt.Println("----", score)
 
 	fmt.Println("--------------------SCORE END-------------------------------------")
 	return int64(score), nil
 }
 
-type LabelPing struct {
-	Label string
-	Value int
-}
-
-func (p *customFilterPlugin) ScoreExtensions() framework.ScoreExtensions {
+func (p *MyK3SPlugin) ScoreExtensions() framework.ScoreExtensions {
 	return p
 }
 
-func (p *customFilterPlugin) NormalizeScore(_ context.Context, _ *framework.CycleState, pod *v1.Pod, scores framework.NodeScoreList) *framework.Status {
+func (p *MyK3SPlugin) NormalizeScore(_ context.Context, _ *framework.CycleState, pod *v1.Pod, scores framework.NodeScoreList) *framework.Status {
 	fmt.Println("----------NORMALIZE  SCORE -------------------------------------")
 	var (
 		highest int64 = 0
@@ -390,18 +192,5 @@ func (p *customFilterPlugin) NormalizeScore(_ context.Context, _ *framework.Cycl
 }
 
 func New(obj runtime.Object, handle framework.Handle) (framework.Plugin, error) {
-	return &customFilterPlugin{handle: handle}, nil
+	return &MyK3SPlugin{handle: handle}, nil
 }
-
-//func pingNode(ip string) (time.Duration, error) {
-//	pinger, err := ping.NewPinger(ip)
-//	if err != nil {
-//		return 0, err
-//	}
-//	pinger.Count = 3
-//	pinger.Timeout = time.Second * 5
-//	pinger.SetPrivileged(true)
-//	pinger.Run()
-//	stats := pinger.Statistics()
-//	return stats.AvgRtt, nil
-//}
